ab31f68fec41aad89ddba1266ba5036f8c97364a
JSoni-Predator <hi@jaysoni.info>
2020-05-18 17:32:41 +0530

    User Model Modified with Email as Primary Key

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    # def test_create_user_with_successful(self):
+    #     """Test creating a new user with an email is successful"""
+    #     username = 'testusername'
+    #     email = 'test@test.com'
+    #     password = 'testpass123'
+    #     user = get_user_model().objects.create_user(
+    #         email=email,
+    #         password=password,
+    #         username=username
+    #     )
+
+    #     self.assertEqual(user.email, email)
+    #     self.assertEqual(user.username, username)
+    #     self.assertTrue(user.check_password(password))
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with an email is successful"""
+        email = 'test@test.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
